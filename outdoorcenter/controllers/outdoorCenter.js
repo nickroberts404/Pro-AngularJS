8bbc40aa002cbafbd8501f64ab814e4fdd835bb9
nickroberts404 <nickroberts404@gmail.com>
2015-02-23 23:09:16 -0600

    Added cart module and cartSummary directive

--- outdoorcenter/controllers/outdoorCenter.js
+++ outdoorcenter/controllers/outdoorCenter.js
@@ -1,12 +1,14 @@
 angular.module("outdoorCenter")
-.controller("outdoorCenterCtrl", function($scope){
+	.constant("dataUrl", "http://localhost:5500/products")
+	.controller("outdoorCenterCtrl", function($scope, $http, dataUrl){
 
-	$scope.data = {
-		products: [
-			{ name: "Product #1", description: "An awesome product", category: "Category #1", price:100},
-			{ name: "Product #2", description: "An awesome product", category: "Category #2", price:110},
-			{ name: "Product #3", description: "An awesome product", category: "Category #3", price:150},
-			{ name: "Product #4", description: "An awesome product", category: "Category #3", price:230}
-			]
-	};
+	$scope.data = {};
+		
+	$http.get(dataUrl)
+		.success(function(data) {
+			$scope.data.products = data;
+		})
+		.error(function(error) {
+			$scope.data.error = error;
+		})
 });
