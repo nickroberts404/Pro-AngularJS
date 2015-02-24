9ccd31fe878f74367234a57211835cefb6471123
nickroberts404 <nickroberts404@gmail.com>
2015-02-23 21:08:38 -0600

    Added controler for product list, added pagination using filters

--- outdoorcenter/controllers/outdoorCenter.js
+++ outdoorcenter/controllers/outdoorCenter.js
@@ -6,7 +6,7 @@ angular.module("outdoorCenter")
 			{ name: "Product #1", description: "An awesome product", category: "Category #1", price:100},
 			{ name: "Product #2", description: "An awesome product", category: "Category #2", price:110},
 			{ name: "Product #3", description: "An awesome product", category: "Category #3", price:150},
-			{ name: "Product #4", description: "An awesome product", category: "Category #4", price:230}
+			{ name: "Product #4", description: "An awesome product", category: "Category #3", price:230}
 			]
 	};
 });
